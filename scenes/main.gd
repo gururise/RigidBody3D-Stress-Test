8c7b8bca05c5962f34b49ee18476946bceb5a5d5
gururise <gururise@gmail.com>
2021-07-12 13:16:52 -0700

    bug fixes and updates

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,11 +3,10 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export(int,20,75000) var num_cubes := 500
-export(int,12,60) var size := 20
-export(float) var x_dist := 2.0
-export(float) var y_dist := 2.0
-export(float) var z_dist := 2.0
+export(int,25,75000) var num_cubes : int = 500
+export(int,1,100) var side_length : int = 15
+export(int,1,400) var impulse : int = 75
+
 #onready var cube : PackedScene = preload("res://scenes/cube.tscn")
 onready var cube_mm : PackedScene = preload("res://scenes/cube_mm.tscn")
 onready var marble : PackedScene = preload("res://scenes/marble.tscn")
@@ -31,27 +30,21 @@ onready var fps_average : float = 0.0
 onready var frames : int = -20  # need to wait a bit before starting to track the fps
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	instanced_marble = marble.instance()
 	add_child(instanced_marble)
 	$CanvasLayer/HSlider.value = num_cubes
 	$CanvasLayer/NumLabel.text = "cubes: " + str(num_cubes) + " (" + str(num_cubes) + ")"
 	# setup multimesh instances
 	mm.set_instance_count(num_cubes)
-	spawnCubes()
+	spawn_cubes()
 
-func _input(event):         
-	if event is InputEventMouseMotion && Input.is_mouse_button_pressed(BUTTON_LEFT):
-		$Camera.rotate_y(deg2rad(-event.relative.x*mouse_sens))
-		var changev=-event.relative.y*mouse_sens
-		if camera_anglev+changev>-50 and camera_anglev+changev<50:
-			camera_anglev+=changev
-			$Camera.rotate_x(deg2rad(changev))
+func _unhandled_input(event) -> void:
 	if event is InputEventKey:
 		if Input.is_key_pressed(KEY_SPACE):
 			launchMarble()
 
-func _process(delta):
+func _process(delta) -> void:
 	timer += delta
 	if timer > TIMER_LIMIT:
 		frames += 1
@@ -64,33 +57,51 @@ func _process(delta):
 			if fps > fps_max:
 				fps_max = fps
 			fps_sum += fps
+# warning-ignore:integer_division
 			fps_average = fps_sum / frames
 			fpsLabel.text = "fps: " + str(fps) + " // " + "min: " + str(fps_min) + " // " + "max: " + str(fps_max) + " // " + "avg: " + str(fps_average)
 
-func _physics_process(delta):
+func _physics_process(_delta) -> void:
 	# update per-instance multimesh transforms on each physics frame
-	for i in range(num_cubes):
+	for i in range(mm.instance_count):
 		mm.set_instance_transform(i,cubeContainer.get_child(i).transform)
 
-func spawnCubes():
+func spawn_cubes() -> void:
+	PhysicsServer.set_active(false)
+	
+	var a = -side_length / 2.0
+	var b = 0
+	var c = a
+	var d = 0
+	
 	for i in range(mm.instance_count):
 		var instanced_cube = cube_mm.instance()
-		var modval = i%size
 # warning-ignore:integer_division
-		if (modval < size/4):
-			instanced_cube.translate(Vector3(modval*x_dist,i/size*y_dist,40))
-# warning-ignore:integer_division
-		elif (modval < size/2):
-			instanced_cube.translate(Vector3(0,i/size*y_dist,40+z_dist+((modval-size/4)*z_dist)))
-# warning-ignore:integer_division
-		elif (modval < size/4*3):
-			instanced_cube.translate(Vector3(size/2-x_dist,i/size*y_dist,40+z_dist+((modval-size/2)*z_dist)))
+		var level = i / (side_length * 4)
+		
+		#reset side a,b,c,d to zero on new level
+		if (i % (side_length * 4) == 0):
+			a = -side_length / 2.0
+			b = 0
+			c = a
+			d = 0
+		
+		if i%4 == 0:
+			instanced_cube.translate(Vector3(a+1,level+0.5,0))			
+			a+=1
+		elif i%4 == 1:
+			instanced_cube.translate(Vector3(-side_length / 2.0 + side_length + 1.0, level + 0.5, b + 1.0))
+			b+=1
+		elif i%4 == 2:
+			instanced_cube.translate(Vector3(c + 1.0, level + 0.5, side_length + 1.0))
+			c+=1
 		else:
-			instanced_cube.translate(Vector3((modval-size/4*3)*x_dist,i/size*y_dist,40+z_dist+size/4*z_dist))
+			instanced_cube.translate(Vector3(-side_length / 2.0, level + 0.5, d + 1.0))
+			d+=1
 		get_node("CubeContainer").add_child(instanced_cube)
 		mm.set_instance_transform(i,instanced_cube.transform)
 
-func resetFPS():
+func resetFPS() -> void:
 	fpsLabel.text = "fps: "
 	fps_min = 9999
 	fps_max = 0
@@ -98,16 +109,20 @@ func resetFPS():
 	fps_average = 0.0
 	frames = -20
 
-func launchMarble():
+func launchMarble() -> void:
 	if not marble_launched:
-		instanced_marble.add_central_force(Vector3(-2000,0,-19000))
+		PhysicsServer.set_active(true)
+		#instanced_marble.translate(Vector3(-side_length/2,0,0))
+		instanced_marble.apply_central_impulse(Vector3(0,0,-impulse))
 		marble_launched = true
+		fps_sum = 0
+		frames = 1
 
-func deleteCubes():
+func deleteCubes() -> void:
 	for c in cubeContainer.get_children():
 		c.free()
 
-func resetAll():
+func resetAll() -> void:
 	resetFPS()
 	# delete marble
 	instanced_marble.free()
@@ -121,22 +136,23 @@ func resetAll():
 	instanced_marble = marble.instance()
 	marble_launched = false
 	add_child(instanced_marble)
-	spawnCubes()
+	spawn_cubes()
 
 
-func _on_HSlider_value_changed(value):
+func _on_HSlider_value_changed(value) -> void:
 	$CanvasLayer/NumLabel.text = "cubes: " + str(value) + " (" + str(num_cubes) + ")"
 
-func _on_Timer_timeout():
+func _on_Timer_timeout() -> void:
 	# Performance monitor does not work for Bullet Physics (we use a workaround) - https://github.com/godotengine/godot/issues/16540
-	#sleepLabel.text = "sleeping: " + str(num_cubes - Performance.get_monitor(Performance.PHYSICS_3D_ACTIVE_OBJECTS) + 1)
+	# sleepLabel.text = "sleeping: " + str(num_cubes - Performance.get_monitor(Performance.PHYSICS_3D_ACTIVE_OBJECTS) + 1)
+	
 	var sleep_count = 0
 	for c in cubeContainer.get_children():
 		if c.sleeping:
 			sleep_count = sleep_count + 1
 	sleepLabel.text = "sleeping: " + str(sleep_count)
 
-func _on_CheckBox_toggled(button_pressed):
+func _on_CheckBox_toggled(button_pressed) -> void:
 	if (button_pressed):
 		$Timer.start()
 		sleepLabel.text = "sleeping: "
